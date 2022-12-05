2c6a116d9bcfc307a70dd7d056265cd0e1f51c5b
Dominik Bauernfeind <dominik.bauernfeind@leaders21.com>
2022-12-05 07:16:19 +0100

    Solve 2022/05 Ruby

--- 2022/Aoc.rb
+++ 2022/Aoc.rb
@@ -2,9 +2,9 @@ require 'net/http'
 require 'tmpdir'
 
 module Aoc
-  def get_input(year:, day:, ignore_cache: false)
+  def get_input(year:, day:, ignore_cache: false, strip: true)
     if (not ignore_cache) && cache_exists?(year, day)
-      return read_cache(year, day)
+      return read_cache(year, day, strip)
     end
     aoc_session = File.read('AOC_SESSION.txt').strip
 
@@ -24,15 +24,15 @@ module Aoc
     File.exist?(cache_path(year, day))
   end
 
-  def read_cache(year, day)
-    File.readlines(cache_path(year, day)).map(&:strip)
+  def read_cache(year, day, strip = true)
+    File.readlines(cache_path(year, day)).map { strip ? _1.strip : _1 }
   end
 
   def write_cache(year, day, input)
     File.write(cache_path(year, day), input)
   end
 
-  def cache_path(year, day)
-    "#{Dir.tmpdir}/aoc_input_#{year}_#{day}.txt"
+  def cache_path(_year, day)
+    "day#{day.to_s.rjust(2, '0')}/input.txt"
   end
 end
---
+++ 2022/day05/ruby/solution.rb
@@ -0,0 +1,74 @@
+require_relative '../../aoc'
+require_relative '../../EnumerableExtensions'
+
+include Aoc
+
+input = get_input(year: 2022, day: 5, strip: false)
+example = <<~EXAMPLE
+      [D]    
+  [N] [C]    
+  [Z] [M] [P]
+   1   2   3 
+
+  move 1 from 2 to 1
+  move 3 from 1 to 3
+  move 2 from 2 to 1
+  move 1 from 1 to 2
+EXAMPLE
+example = example.split("\n")#.map(&:strip)
+
+class String
+  def integers
+    self.tr('^0-9', ' ').split(' ').map(&:strip).reject(&:nil?).map(&:to_i)
+  end
+end
+def part1(input)
+  crates_input, moves_input = input.chunk_by { _1 == "\n" || _1 == '' }
+
+  stacked = Hash.new { |h,k| h[k] = [] }
+  crates_input
+    .map { |line| line.split('').each_slice(4).map { _1[1] } }
+    .reverse
+    .drop(1)
+    .each do |stacks|
+      stacks.each.with_index do |item, index|
+        stacked[index + 1].push(item) unless item == ' '
+      end
+  end
+
+  moves_input.drop(1).map(&:integers).each do |n, from, to|
+    n.times do
+      stacked[to] << stacked[from].pop
+    end
+  end
+
+  stacked.values.map(&:pop).join('')
+end
+
+def part2(input)
+  crates_input, moves_input = input.chunk_by { _1 == "\n" || _1 == '' }
+
+  stacked = Hash.new { |h,k| h[k] = [] }
+  crates_input
+    .map { |line| line.split('').each_slice(4).map { _1[1] } }
+    .reverse
+    .drop(1)
+    .each do |stacks|
+    stacks.each.with_index do |item, index|
+      stacked[index + 1].push(item) unless item == ' '
+    end
+  end
+
+  moves_input.drop(1).map(&:integers).each do |n, from, to|
+    moving = []
+    n.times { moving << stacked[from].pop }
+    moving.reverse.each { stacked[to] << _1 }
+  end
+
+  stacked.values.map(&:pop).join('')
+end
+
+puts "Part 1 (Example): #{part1(example)}"
+puts "Part 1: #{part1(input)}"
+puts "Part 2 (Example): #{part2(example)}"
+puts "Part 2: #{part2(input)}"
