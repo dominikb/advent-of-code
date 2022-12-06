fabedb29f1a8e78a34d4c3c6245381f0cc7dc43b
Dominik Bauernfeind <dominik.bauernfeind@leaders21.com>
2022-12-06 06:56:05 +0100

    Refactor Ruby 2022 day 6

--- 2022/day06/ruby/solution.rb
+++ 2022/day06/ruby/solution.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
 require_relative '../../../utils/ruby/Aoc'
 
 input = Aoc.get_input(year: 2022, day: 6)
 example = <<~EXAMPLE
-mjqjpqmgbljsphdztnvjfqwrcgsmlb
-bvwbjplbgvbhsrlpgdmjqwftvncz
-nppdvjthqldpwncqszvftbrmjlhg
-nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
-zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+  mjqjpqmgbljsphdztnvjfqwrcgsmlb
+  bvwbjplbgvbhsrlpgdmjqwftvncz
+  nppdvjthqldpwncqszvftbrmjlhg
+  nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+  zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
 EXAMPLE
 example = example.split("\n")
 
 def uniq_seq_index(signal, len)
   (0..).each do |n|
-    if signal[n...(n + len)].chars.uniq.count == len
-      # puts signal[n...(n + len)]
-      return n + len
-    end
+    return n + len if signal[n, len].chars.uniq.count == len
   end
 end
+
+def uniq_seq_index2(signal, len)
+  signal.chars.each_cons(len).find_index { _1.uniq.count == len } + len
+end
+
 def part1(input)
-  input.map { uniq_seq_index(_1, 4) }.join(', ')
+  input.map { uniq_seq_index2(_1, 4) }.join(', ')
 end
 
 def part2(input)
