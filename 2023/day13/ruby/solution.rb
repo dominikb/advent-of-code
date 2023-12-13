720e41480b7df8419a3c1c9227b9fb2f00f0edab
Dominik Bauernfeind <dominik.bauernfeind@leaders21.com>
2023-12-13 12:05:11 +0100

    2023-13 Ruby part2

--- 2023/day13/ruby/solution.rb
+++ 2023/day13/ruby/solution.rb
@@ -25,27 +25,22 @@ def distance(a, b)
     a.chars.zip(b.chars).count { _1 != _2 }
 end
 
-ALLOWED_DISTANCE = 0
-
-def verify(mirror_index, input, prev_distance = 0)
-    total_distance = prev_distance
+def verify(mirror_index, input)
+    total_distance = 0
     (0..mirror_index).each do |i|
         a = mirror_index - i
         b = mirror_index + i + 1
-        
-        return true if a < 0 || b >= input.length
+
+        return total_distance == ALLOWED_DISTANCE if (a < 0 || b >= input.length)
         total_distance += distance(input[a], input[b])
         return false if total_distance > ALLOWED_DISTANCE
     end
-    total_distance <= ALLOWED_DISTANCE
+    total_distance == ALLOWED_DISTANCE
 end
 
 def horizontal(input)
     (0..(input.length - 2)).each do |i|
-        d = distance(input[i], input[i + 1])
-        if d <= ALLOWED_DISTANCE
-            return i + 1 if verify(i, input, d)
-        end
+        return i + 1 if verify(i, input)# && distance(input[i], input[i + 1]) == ALLOWED_DISTANCE
     end
     nil
 end
@@ -57,19 +52,22 @@ end
 
 def part1(input)
     input.map do |grid|
-        # puts grid.inspect
-        # puts
         h = horizontal(grid)
         next h * 100 if h
         vertical(grid)
-        # [horizontal(grid), vertical(grid)]
-    end.sum
+    end
+    .sum
 end
 
 def part2(input)
+    part1(input)
 end
 
+
+ALLOWED_DISTANCE = 0
 puts "Part 1 (Example): #{part1(example)}"
 puts "Part 1: #{part1(input)}"
-# puts "Part 2 (Example): #{part2(example)}"
-# puts "Part 2: #{part2(input)}"
+
+ALLOWED_DISTANCE = 1
+puts "Part 2 (Example): #{part2(example)}"
+puts "Part 2: #{part2(input)}"
